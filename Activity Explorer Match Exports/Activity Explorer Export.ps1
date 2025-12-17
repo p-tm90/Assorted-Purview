7ce8673d1cad379f3822111f4a2a47b45e299b8c
p-tm90 <239238749+p-tm90@users.noreply.github.com>
2025-12-16 22:52:43 -0800

    Regular updates.

--- Activity Explorer Match Exports/Activity Explorer Export.ps1
+++ Activity Explorer Match Exports/Activity Explorer Export.ps1
@@ -4,6 +4,6 @@ Set-Location $Location
 $OutputsPath = "$Location\Outputs"
 
 #Importing core functions & initiate connection to Purview.
-. .\Dependencies\Functions.ps1
+. .\Dependencies\ModuleConnectionFunctions.ps1
 Configure-MicrosoftModules -TypeOfConfiguration Purview -InstallScope CurrentUser
 
---
+++ SIT Exporter/MSFT_Rulepack_Export.ps1
@@ -0,0 +1,1 @@
+. .\Dependencies\ModuleConnectionFunctions.ps1
