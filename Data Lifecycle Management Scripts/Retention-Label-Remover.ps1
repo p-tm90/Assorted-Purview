1d77d7638829ddd50e525abe9a37b76b714eb7fc
p-tm90 <239238749+p-tm90@users.noreply.github.com>
2026-01-11 22:11:08 -0800

    Configuring retention rule deletion script and pre-req components.

---
+++ Data Lifecycle Management Scripts/Retention-Label-Remover.ps1
@@ -0,0 +1,21 @@
+. .\Prerequisites.ps1
+
+Configure-MicrosoftModules -TypeOfConfiguration Purview -InstallScope CurrentUser
+
+$LabelName = Read-Host "Enter Retention Label Name to Remove"
+$RetentionRules = Get-RetentionComplianceRule | Where-Object {
+    $_.ApplyComplianceTag -like "*$($LabelName)" -or `
+    $_.ComplianceTagProperty -like "*$($LabelName)" -or `
+    $_.PublishComplianceTag -like "*$($LabelName)"
+}
+
+$PolicyNames = @()
+Foreach ($Rule in $RetentionRules) {
+    $Policy = Get-RetentionCompliancePolicy -Identity $Rule.Policy
+    $PolicyNames += $Policy
+}
+
+Write-Host "Policies tied to the label '$LabelName':"
+Foreach ($PolicyName in $PolicyNames){
+        Write-Host "• Policy Name: $($PolicyName.Name)`n•Policy GUID: $($PolicyName.GUID)`n" -ForegroundColor Cyan
+}
